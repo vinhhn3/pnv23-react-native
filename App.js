ef427e67a030ad5537f0219658989151de0be729
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-09-30 15:10:36 +0700

    Basic components

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello PNV23-React Native</Text>
+      <Button onPress={increaseCount} title="Increase Count"></Button>
+      <Text style={styles.count}>Count: {count}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#FF0000", // Red color
+    fontSize: 50,
+    fontStyle: "italic",
+  },
+  count: {
+    fontSize: 30,
   },
 });
